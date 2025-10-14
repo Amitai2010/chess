9ea1ee8c55abad57ee54a586ceb1e03f2e1df65f
Amitai <amitaikeshet2010@gmail.com>
2025-10-14 16:11:23 +0300

    pawn logic needs fixes

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -12,7 +12,13 @@ class Pawn < Piece
     valid = []
 
     if @color == 'white'
-      
+      if position[0] < 6 && position[0] > 0 && board.game_board[position[0] + 1][position[1]] == ' '
+        valid.push([position[0] + 1, position[1]])
+      elsif position[0] == 6 && board.game_board[position[0] + 1][position[1]] == ' '
+        board.game_board[position[0] + 2][position[1]] == ' ' ? valid.push([position[0] + 1, position[1]]) && valid.push([position[0] + 2, position[1]]) : valid.push([position[0] + 1, position[1]])
+      end
+      valid.push([position[0] - 1, position[1] - 1]) if board.game_board[position[0] + 1][position[1] - 1] != ' '
+      valid.push([position[0] - 1, position[1] + 1]) if board.game_board[position[0] + 1][position[1] + 1] != ' '
     end
   end
 end
